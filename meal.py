bdcae19e892512426a2b763be60987f3094f75c7
SebbyLam <159206469+SebbyLam@users.noreply.github.com>
2026-02-19 12:31:39 +0000

    Meal.py

--- meal.py
+++ meal.py
@@ -1,14 +1,24 @@
 def main():
     time_str = input("what time is it")
+    total_min = convert(time_str)
+
+    if 480 <= total_min <= 660:
+        print("breakfast")
+
+    elif 720 <= total_min <= 810:
+        print("lunch")
+
+    else:
+        print("Not meal time.")
 
 
 def convert(time):
-    hours, minutes = time_str.split(":")
+    hours, minutes = time.split(":")
     hours = int(hours)
     minutes = int(minutes)
+    totalmin = hours * 60 + minutes
+    return totalmin
 
 
-totalmin = hours * 60 + minutes
-
-if totalmin >= 480 and totalmin <= 660:
-    print("breakfast")
+if __name__ == "__main__":
+    main()
