daa39d8344314f284bd9c2d76f81acd0e88dae53
SebbyLam <159206469+SebbyLam@users.noreply.github.com>
2026-02-20 00:54:32 +0000

    WIP fix camel

---
+++ camel.py
@@ -0,0 +1,16 @@
+def indexC(char):
+    capitals = []
+    for index, char in enumerate():
+        if char.isupper():
+            capitals.append(index)
+        return capitals
+
+
+bl = input("speak")
+blah = indexC(bl)
+print(blah)
+
+
+# Actual code result = input("speak")
+# indexed = indexC(result)
+# split
