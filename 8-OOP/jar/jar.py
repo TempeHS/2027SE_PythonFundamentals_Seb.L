adcde0968e0a98af964e1c676d20e0f9cf3ad90c
SebbyLam <159206469+SebbyLam@users.noreply.github.com>
2026-04-27 09:30:14 +0000

    8.2 can wait

---
+++ 8-OOP/jar/jar.py
@@ -0,0 +1,17 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError("Invalid capacity")
+        self._capacity = capacity
+
+    def __str__(self): ...
+
+    def deposit(self, n): ...
+
+    def withdraw(self, n): ...
+
+    @property
+    def capacity(self): ...
+
+    @property
+    def size(self): ...
