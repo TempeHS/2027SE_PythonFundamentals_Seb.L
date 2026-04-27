c67079df7612cee2793758fc911fa0fc3b09acfb
SebbyLam <159206469+SebbyLam@users.noreply.github.com>
2026-04-27 23:41:20 +0000

    6.1 done add test file to  6.1

---
+++ 6-FileIO/lines/lines.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python lines.py filename.py")
+
+    filename = sys.argv[1]
+
+    # 2) Validate file extension
+    if not filename.endswith(".py"):
+        sys.exit("Not a Python file")
+
+    # 3) Open file safely
+    try:
+        with open(filename, "r") as file:
+            # TODO: count only real code lines
+            count = 0
+            for line in file:
+                # TODO: ignore blank lines and comment-only lines
+                pass
+
+        print(count)
+
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+
+if __name__ == "__main__":
+    main()
