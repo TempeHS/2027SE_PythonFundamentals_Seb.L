ad92764a1b91cafff51278038c980b94b39aeab2
SebbyLam <159206469+SebbyLam@users.noreply.github.com>
2026-04-30 20:07:14 +0000

    6.2 done

--- 6-FileIO/scourgify/scourgify.py
+++ 6-FileIO/scourgify/scourgify.py
@@ -7,10 +7,10 @@ filename = sys.argv[1]
 if not filename.lower().endswith(".csv"):
     sys.exit("Not a csv")
 try:
-    with open(filename) as file
+    with open(filename, newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
             last, first = [part.strip() for part in row["name"].split(",")]
-            rows.append({"first": first,
-                "last": last,
-                "house": row["house"].strip()})
+            rows.append({"first": first, "last": last, "house": row["house"].strip()})
+except FileNotFoundError:
+    sys.exit("File does not exist")
